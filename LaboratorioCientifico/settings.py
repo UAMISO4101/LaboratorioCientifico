1ae22b5dd277d43f633b70f50e745511cfe6c0c6
Gabriel Zapata <gab.zapata@uniandes.edu.co>
2017-03-24 10:51:18 -0500

    Ejecuta la transaccion afectando bodegas y validaciones de tipos de transaccion

--- LaboratorioCientifico/settings.py
+++ LaboratorioCientifico/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'laboratorio',
         'USER': 'postgres',
-        'PASSWORD': '1234',
+        'PASSWORD': '',
         'HOST': 'localhost',
         'PORT': '5432',
     }
