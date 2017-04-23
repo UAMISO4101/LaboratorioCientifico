6b38c2c0d6461efe17e4286d1a666aa2fbc64383
dalthviz <d.althviz10@uniandes.edu.co>
2017-04-23 03:12:05 -0500

    Correción errores de estilo.

--- laboratorio/test/tests_aprobacion_ordenes.py
+++ laboratorio/test/tests_aprobacion_ordenes.py
@@ -7,7 +7,7 @@ from laboratorio.models import Tipo, OrdenPedido, ComentarioOrden
 
 """Clase AprobacionOrdenesTestCase.
 
-Clase encargada de generar las pruebas unitarias para aprobacion de ordenes de pedido y comentarios.
+Clase encargada de generar las pruebas unitarias para aprobacion de ordenes de pedido y comentarios
 
 """
 class AprobacionOrdenesTestCase(TestCase):
