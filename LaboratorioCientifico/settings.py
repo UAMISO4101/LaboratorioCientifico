97d1b0e223f80469365c8aeb2fdbd8d62285bf5f
yedgar <yedgar@gmail.com>
2017-05-20 15:28:57 -0500

    Codeship

--- LaboratorioCientifico/settings.py
+++ LaboratorioCientifico/settings.py
@@ -129,7 +129,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/images/'
-STATIC_ROOT = '/staticfiles/'
+STATIC_ROOT = os.path.join(BASE_DIR,'static')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'images')
 ]
